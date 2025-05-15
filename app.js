3134705bd03d53e42e17bfad69ed1d89c88848ba
Abdul Moiz <moiz05366@gmail.com>
2025-05-16 03:44:58 +0500

    updated

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const inp = document.getElementById("textarea");
 const btn = document.getElementById("submit-btn");
+const clearBtn = document.getElementById("clear-btn");
+const mcqsContainer = document.getElementById("mcqs-container");
 
 const clickHandler = () => {
     const questions = [];
     let question = {};
+    let options = [];
     const val = inp.value.split("\n");
     let count = 0;
     for (let i = 0; i < val.length; i++) {
         if (val[i] === "") {
             questions.push(question);
+            question.options = options;
             question = {};
+            options = [];
             count = 0;
         } else {
             if (val[i].includes("?")) {
@@ -19,15 +24,41 @@ const clickHandler = () => {
                 if (val[i].includes("*")) {
                     const ans = val[i].slice(0, -1);
                     question.ans = ans;
-                    question[`option${count}`] = ans;
+                    options.push(ans);
                 } else {
-                    question[`option${count}`] = val[i];
+                    options.push(val[i]);
                 }
             }
         }
     }
 
-    console.log(questions);
+    return questions;
 }
 
-btn.addEventListener("click", clickHandler);
+const extractMcqs = (questions) => {
+    mcqsContainer.innerHTML = "";
+    for (let question of questions) {
+        mcqsContainer.innerHTML += `<div class="mcq-container">
+            <div class="question">Q1: ${question.question}</div>
+            <div class="options">
+                <div class="option correct-answer">A. ${question.ans}</div>
+                ${question.options.map((opt, idx) => {
+            return `<div class="option">${idx + 1}. ${opt}</div>`
+        }).join("")}
+            </div>
+        </div>`;
+    }
+}
+
+const clear = () => {
+    inp.value = "";
+    mcqsContainer.innerHTML = "";
+}
+
+const main = () => {
+    const questions = clickHandler();
+    extractMcqs(questions);
+}
+
+btn.addEventListener("click", main);
+document.getElementById("clear-btn").addEventListener("click", clear);
